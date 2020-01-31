a7ea572ba99e72e1dd09c4260140721dfeb1514e
Mihail Galkov <gaus.spb@gmail.com>
2020-01-31 12:32:18 +0300

    Test vesion 1, changing permission of gradlew

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -4,16 +4,21 @@ import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.exactText;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 
 
 public class CardDeliveryTest {
+
     @Test
     void shouldSumbitRequest() {
         open("http://localhost:9999/");
@@ -35,6 +40,13 @@ public class CardDeliveryTest {
         $("[name='phone']").setValue("+79123456789");
         $(".checkbox__box").click();
         $("[role=button][type=button].button").click();
-        $(withText("Успешно")).waitUntil(visible,5000);
+        $(withText("Успешно")).waitUntil(visible,15000);
+    }
+
+    @Test
+    void shouldAutoFillCity(){
+        open("http://localhost:9999/");
+        $("[placeholder]").setValue("аб");
+        $(".menu-item__control").find("Хабаровск");
     }
 }
